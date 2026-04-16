7bfdc95999fb3dde09c5f78f32eb7134281a6f21
Theo Beers <theo.beers@fu-berlin.de>
2026-04-16 07:56:45 -0400

    update dependencies

--- js/new-search.js
+++ js/new-search.js
@@ -1,27 +1,27 @@
 /* global calcIslamic */
 
 // Global consts for new DOM elements
-const newMonthField = document.getElementById('new-select-month')
-const newDayField = document.getElementById('new-select-day')
-const newStartYearField = document.getElementById('new-select-start-year')
-const newEndYearField = document.getElementById('new-select-end-year')
-const newWeekdayField = document.getElementById('new-select-weekday')
-const newSubmitButton = document.getElementById('new-submit-button')
-const newResetButton = document.getElementById('new-reset-button')
-const newResultsTextField = document.getElementById('new-results-text')
+const newMonthField = document.getElementById("new-select-month");
+const newDayField = document.getElementById("new-select-day");
+const newStartYearField = document.getElementById("new-select-start-year");
+const newEndYearField = document.getElementById("new-select-end-year");
+const newWeekdayField = document.getElementById("new-select-weekday");
+const newSubmitButton = document.getElementById("new-submit-button");
+const newResetButton = document.getElementById("new-reset-button");
+const newResultsTextField = document.getElementById("new-results-text");
 
 // Function to search for matching years
-function experimentalSearch () {
+function experimentalSearch() {
   // Consts for submitted values
-  const submittedMonthIndex = newMonthField.selectedIndex
-  const submittedDay = newDayField.value
-  const submittedStartYear = Number(newStartYearField.value)
-  const submittedEndYear = Number(newEndYearField.value)
-  const submittedWeekday = newWeekdayField.value
+  const submittedMonthIndex = newMonthField.selectedIndex;
+  const submittedDay = newDayField.value;
+  const submittedStartYear = Number(newStartYearField.value);
+  const submittedEndYear = Number(newEndYearField.value);
+  const submittedWeekday = newWeekdayField.value;
 
   // Abort if any field has been left empty
   if (!submittedDay || !submittedStartYear || !submittedEndYear) {
-    return
+    return;
   }
 
   // Abort if any submitted value is out of bounds
@@ -33,38 +33,36 @@ function experimentalSearch () {
     submittedEndYear < 100 ||
     submittedEndYear > 1400
   ) {
-    newResultsTextField.innerText =
-      'At least one submitted value was out of bounds!'
-    return
+    newResultsTextField.innerText = "At least one submitted value was out of bounds!";
+    return;
   }
 
   // Abort if start year is later than end year
   if (submittedStartYear > submittedEndYear) {
-    newResultsTextField.innerText =
-      'The start year is later than the end year…?'
-    return
+    newResultsTextField.innerText = "The start year is later than the end year…?";
+    return;
   }
 
   // Limit year range to 300 (technically 301 inclusive)
   // Abort if this is bad
   if (submittedEndYear - submittedStartYear > 300) {
-    newResultsTextField.innerText = 'The year range is too large!'
-    return
+    newResultsTextField.innerText = "The year range is too large!";
+    return;
   }
 
   // Empty array to populate with results
-  const resultsList = []
+  const resultsList = [];
 
   // Iterate over the desired inclusive range of years
   for (let i = submittedStartYear; i <= submittedEndYear; i++) {
     // Set hidden Islamic date values
     // The month and day are reset each time, since they can fluctuate
-    document.islamic.year.value = i
-    document.islamic.month.selectedIndex = submittedMonthIndex
-    document.islamic.day.value = submittedDay
+    document.islamic.year.value = i;
+    document.islamic.month.selectedIndex = submittedMonthIndex;
+    document.islamic.day.value = submittedDay;
 
     // Now that the hidden date is set, run the big calc function
-    calcIslamic()
+    calcIslamic();
 
     // Make sure the hidden month and day haven't been changed
     // I'm not 100% sure this works
@@ -75,29 +73,29 @@ function experimentalSearch () {
       // If calculated weekday == submitted weekday
       if (document.gregorian.wday.value === submittedWeekday) {
         // Push the year to the results array
-        resultsList.push(i)
+        resultsList.push(i);
       }
     }
   }
 
   // Display results, if any
   if (resultsList.length > 0) {
-    newResultsTextField.innerText = resultsList.join(', ')
+    newResultsTextField.innerText = resultsList.join(", ");
   } else {
-    newResultsTextField.innerText = 'No matches were found.'
+    newResultsTextField.innerText = "No matches were found.";
   }
 }
 
 // Function to clear all fields
-function newReset () {
-  newMonthField.selectedIndex = 0
-  newDayField.value = ''
-  newStartYearField.value = ''
-  newEndYearField.value = ''
-  newWeekdayField.selectedIndex = 0
-  newResultsTextField.innerText = '…'
+function newReset() {
+  newMonthField.selectedIndex = 0;
+  newDayField.value = "";
+  newStartYearField.value = "";
+  newEndYearField.value = "";
+  newWeekdayField.selectedIndex = 0;
+  newResultsTextField.innerText = "…";
 }
 
 // Event listeners for new functions
-newSubmitButton.addEventListener('click', experimentalSearch)
-newResetButton.addEventListener('click', newReset)
+newSubmitButton.addEventListener("click", experimentalSearch);
+newResetButton.addEventListener("click", newReset);
